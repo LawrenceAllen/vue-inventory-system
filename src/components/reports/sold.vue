578ba406d3bb950dc928c2b04d61ecf6e8e09610
LawrenceAllen <bolanos.allenlawrence@gmail.com>
2022-06-14 16:29:35 +0800

    Updated showing of months where items were sold. Added month traversal functionality.

--- src/components/reports/sold.vue
+++ src/components/reports/sold.vue
@@ -2,14 +2,12 @@
 import CustomText from '../../common/custom-text.vue';
 
 interface props {
-  currentMonth: string
   itemsSoldCurrentMonth: number
   salesCurrentMonth: number
   profitCurrentMonth: number
 }
 
 const props = withDefaults(defineProps<props>(), {
-  currentMonth: '',
   itemsSoldCurrentMonth: 0,
   salesCurrentMonth: 0,
   profitCurrentMonth: 0
@@ -18,19 +16,17 @@ const props = withDefaults(defineProps<props>(), {
 
 <template>
   <div class="bg-emerald-500 w-full rounded p-4">
-    <CustomText class="text-xl" :isPrimary="true" :value="props.currentMonth"/>
     <div class="flex justify-between items-center my-2">
-      <CustomText :isPrimary="true" :value="'Items sold:'"/>
+      <CustomText class="text-lg" :isPrimary="true" :value="'Items sold:'"/>
       <CustomText :isPrimary="true" :value="String(props.itemsSoldCurrentMonth)"/>
     </div>
     <div class="flex justify-between items-center my-2">
-      <CustomText :isPrimary="true" :value="'Revenue: '"/>
+      <CustomText class="text-lg" :isPrimary="true" :value="'Revenue: '"/>
       <CustomText :isPrimary="true" :value="'₱' + props.salesCurrentMonth"/>
     </div>
     <div class="flex justify-between items-center my-2">
-      <CustomText :isPrimary="true" :value="'Profit: '"/>
+      <CustomText class="text-lg" :isPrimary="true" :value="'Profit: '"/>
       <CustomText :isPrimary="true" :value="'₱' + props.profitCurrentMonth"/>
     </div>
-    
   </div>
 </template>
--- src/pages/reports/reports.vue
+++ src/pages/reports/reports.vue
@@ -3,15 +3,18 @@ import { onSnapshot } from '@firebase/firestore';
 import { ref, watchEffect } from 'vue';
 import { soldColRef } from '../../../firebase-config';
 import Sold from '../../components/reports/sold.vue'
+import CustomText from '../../common/custom-text.vue';
+import CustomButton from '../../common/custom-button.vue';
 
 const soldItemsList = ref([])
 const sortedSoldItemsList = ref([])
 const currentMonthSoldItems = ref([])
-const currentMonthRetailPrice = ref([])
-const currentMonthItemProfit = ref([])
+const itemsSoldMonthList = ref([])
 const currentMonthProfit = ref(0)
 const currentMonthSales = ref(0)
-const monthYear = ref('')
+const latestMonth = ref('')
+const reducedMonthList = ref([])
+const index = ref(0)
 
 onSnapshot(soldColRef, (snap) => {
   // returns the items from the firebase database
@@ -19,44 +22,89 @@ onSnapshot(soldColRef, (snap) => {
 })
 
 watchEffect(() => {
-  // sorts the items by date
-  sortedSoldItemsList.value = soldItemsList.value.sort((a, b) => Date.parse(a.date_sold) - Date.parse(b.date_sold))
+  // returns the latest items that were sold
+  sortedSoldItemsList.value = soldItemsList.value.sort((a, b) => Date.parse(b.date_sold) - Date.parse(a.date_sold))
 
-  const currentMonth = new Date().toLocaleDateString('en-US', {month: "long"})
-  const currentYear = new Date().toLocaleDateString('en-US', {year: "numeric"})
-  // sets the current Month and Year
-  monthYear.value = currentMonth + " " + currentYear
+  // returns the latest MONTH and YEAR from the items that were sold
+  if (sortedSoldItemsList.value[0] !== undefined) {
+    const latestDate = sortedSoldItemsList.value[0]
+    if (latestMonth.value === '') {
+      latestMonth.value = latestDate.date_sold.split(" ").at(0) + " " + latestDate.date_sold.split(" ").at(2)
+    }
+  }
 
-  // returns the MONTH and YEAR from the date_sold property (June 13, 2022)
+  // returns the items from the latest month
   currentMonthSoldItems.value = sortedSoldItemsList.value.map((e) => ({
     month: e.date_sold.split(" ").at(0) + " " + e.date_sold.split(" ").at(2),
     retail_price: e.retail_price,
     item_profit: e.item_profit
   }))
 
-  // sets the value of currentMonthSoldItems to items sold on current month
-  currentMonthSoldItems.value = currentMonthSoldItems.value.filter(e => e.month === monthYear.value)
+  // returns the month from the soldItemsList
+  itemsSoldMonthList.value = sortedSoldItemsList.value.map((e) => e.date_sold.split(" ").at(0) + " " + e.date_sold.split(" ").at(2))
 
-  // sets the value of currentMonthRetailPrice to retail price of items from currentMonthSoldItems
-  currentMonthRetailPrice.value = currentMonthSoldItems.value.map(e => e.retail_price)
+  // reduces the size of months from itemsSoldMonthList
+  if (itemsSoldMonthList.value.length !== 0) {
+    let tempArray = []
+    let temp = itemsSoldMonthList.value[0]
+    tempArray.push(temp)
 
-  // sets the value of currentMonthItemProfit to item profit of items from currentMonthSoldItems
-  currentMonthItemProfit.value = currentMonthSoldItems.value.map(e => e.item_profit)
+    itemsSoldMonthList.value.forEach(month => {
+      if (temp !== month) {
+        temp = month
+        tempArray.push(temp)
+      }
+    })
+
+    reducedMonthList.value = tempArray
+    temp = ''
+    tempArray = []
+  }
+
+  // returns the items based on the latestMonth value
+  currentMonthSoldItems.value = currentMonthSoldItems.value.filter(e => e.month === latestMonth.value)
 
   // returns the sum of retail price from the items on currentMonthRetailPrice
-  currentMonthSales.value = currentMonthRetailPrice.value.reduce((a, b) => a + b, 0)
+  const currentMonthRetailPrice = currentMonthSoldItems.value.map(e => e.retail_price)
+  currentMonthSales.value = currentMonthRetailPrice.reduce((a, b) => a + b, 0)
 
-  // returns the sum of ritem profit from the items on currentMonthItemProfit
-  currentMonthProfit.value = currentMonthItemProfit.value.reduce((a, b) => a + b, 0)
+  // returns the sum of item profit from the items on currentMonthItemProfit
+  const currentMonthItemProfit = currentMonthSoldItems.value.map(e => e.item_profit)
+  currentMonthProfit.value = currentMonthItemProfit.reduce((a, b) => a + b, 0)
+
 })
+
+const traverseMonths = (string) => {
+  if (reducedMonthList.value.length !== 0) {
+    if (string === "previous") {
+      index.value += 1
+      if (reducedMonthList.value[index.value] === undefined) {
+        index.value -= 1
+      } else {
+        latestMonth.value = reducedMonthList.value[index.value]
+      }
+    } else {
+      index.value -= 1
+      if (reducedMonthList.value[index.value] === undefined) {
+        index.value += 1
+      } else {
+        latestMonth.value = reducedMonthList.value[index.value]
+      }
+    }
+  }
+}
 
 </script>
 
 <template>
   <div class="flex flex-col justify-center items-center gap-4 w-full">
     <p class="text-3xl text-white">Reports</p>
+    <div class="flex justify-between items-center bg-emerald-500 w-full rounded p-4">
+      <CustomText class="text-2xl" :isPrimary="true" :value="'<'" @click="() => traverseMonths('previous')"/>
+      <CustomButton class="text-xl text-center rounded w-40 drop-shadow" :buttonType="'cancel'" :value="latestMonth"/>
+      <CustomText class="text-2xl" :isPrimary="true" :value="'>'" @click="() => traverseMonths('next')"/>
+    </div>
     <Sold 
-      :currentMonth="monthYear"
       :itemsSoldCurrentMonth="currentMonthSoldItems.length" 
       :salesCurrentMonth="currentMonthSales"
       :profitCurrentMonth="currentMonthProfit"
